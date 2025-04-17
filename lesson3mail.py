87fcc4d0cd35c678b668c741f23d99a162625a4e
Polina Vasilyeva <Polinova31@gmail.com>
2025-04-17 17:53:47 +0300

    Add files via upload

--- lesson3mail.py
+++ lesson3mail.py
@@ -3,10 +3,12 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
+email_from = 'Polinova31@yandex.ru'
+email_to = 'Politovmaksim01@yandex.ru'
 
 letter = """\
-From: Polinova31@yandex.ru
-To: Politovmaksim01@yandex.ru
+From: {0}
+To: {1}
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8";
 
@@ -25,7 +27,7 @@ Content-Type: text/plain; charset="UTF-8";
 Все проекты — они же решение наших задачек — можно разместить на твоём GitHub. Работодатели такое оценят. 
 
 Регистрируйся → %website%  
-На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""
+На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл.""".format(email_from, email_to)
 
 letter = letter.replace('%friend_name%', 'Максим')
 letter = letter.replace('%my_name%', 'Полина')
@@ -34,9 +36,6 @@ letter = letter.replace('%website%', 'https://dvmn.org/profession-ref-program/co
 letter = letter.encode("UTF-8")
 
 
-
-email_from = 'Polinova31@yandex.ru'
-email_to = 'Politovmaksim01@yandex.ru'
 message = letter
 server = smtplib.SMTP_SSL('smtp.yandex.com', 465) 
 pogin = os.getenv('LOGIN')
@@ -44,3 +43,5 @@ password = os.getenv('PASSWORD')
 server.login(login,password)
 server.sendmail(email_from, email_to, message)
 server.quit()
+
+
